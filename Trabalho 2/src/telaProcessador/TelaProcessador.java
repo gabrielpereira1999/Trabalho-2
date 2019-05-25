88566852a0da54929a76bfe14bf7d26a689a3aa7
GabrielPereira <pereira.gabrielf99@gmail.com>
2019-05-25 15:52:46 -0300

    Gabriel
    seila

--- Trabalho 2/src/telaProcessador/TelaProcessador.java
+++ Trabalho 2/src/telaProcessador/TelaProcessador.java
@@ -27,7 +27,7 @@ public class TelaProcessador extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtPesquisa;
-	
+
 	public TelaProcessador() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 783, 637);
@@ -35,73 +35,73 @@ public class TelaProcessador extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-contentPane.setLayout(null);
-		
+		contentPane.setLayout(null);
+
 		JLabel lblMonster = new JLabel("MONSTER");
 		lblMonster.setFont(new Font("Tahoma", Font.PLAIN, 40));
 		lblMonster.setBounds(10, 11, 193, 49);
 		contentPane.add(lblMonster);
-		
+
 		JLabel lblInformtica = new JLabel("Inform\u00E1tica");
 		lblInformtica.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		lblInformtica.setBounds(52, 58, 90, 22);
 		contentPane.add(lblInformtica);
-		
+
 		JButton btnMinhaConta = new JButton("Minha conta");
 		btnMinhaConta.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnMinhaConta.setBounds(510, 11, 120, 30);
 		contentPane.add(btnMinhaConta);
-		
+
 		JButton btnMeuCarrinho = new JButton("Meu carrinho");
 		btnMeuCarrinho.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnMeuCarrinho.setBounds(640, 11, 120, 30);
 		contentPane.add(btnMeuCarrinho);
-		
+
 		JButton btnDeslogar = new JButton("Deslogar");
 		btnDeslogar.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnDeslogar.setBounds(380, 11, 120, 30);
 		contentPane.add(btnDeslogar);
-		
-		//Cria ícone para botão pesquisa
+
+		// Cria ícone para botão pesquisa
 		ImageIcon iconPesquisa = new ImageIcon(TelaHomePage.class.getResource("iconPesquisa.jpg"));
 		Image temp = iconPesquisa.getImage();
 		Image newimg = temp.getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon resizedIconPesquisa = new ImageIcon(newimg);
-		
+
 		JButton btnColocarimagemLupa = new JButton(resizedIconPesquisa);
 		btnColocarimagemLupa.setBounds(730, 58, 30, 30);
 		contentPane.add(btnColocarimagemLupa);
-		
+
 		txtPesquisa = new JTextField();
 		txtPesquisa.setBounds(380, 58, 340, 30);
 		contentPane.add(txtPesquisa);
 		txtPesquisa.setColumns(10);
-		
+
 		JButton btnPlacaDeVdeo = new JButton("Placa de V\u00EDdeo");
 		btnPlacaDeVdeo.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnPlacaDeVdeo.setBounds(10, 131, 142, 30);
 		contentPane.add(btnPlacaDeVdeo);
-		
+
 		JButton btnProcessador = new JButton("Processador");
 		btnProcessador.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnProcessador.setBounds(162, 131, 142, 30);
 		contentPane.add(btnProcessador);
-		
+
 		JButton btnHddSsd = new JButton("HDD/SSD");
 		btnHddSsd.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnHddSsd.setBounds(314, 131, 142, 30);
 		contentPane.add(btnHddSsd);
-		
+
 		JButton btnMemriaRam = new JButton("Mem\u00F3ria RAM");
 		btnMemriaRam.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnMemriaRam.setBounds(466, 131, 142, 30);
 		contentPane.add(btnMemriaRam);
-		
+
 		JButton btnPerifricos = new JButton("Perif\u00E9ricos");
 		btnPerifricos.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnPerifricos.setBounds(618, 131, 142, 30);
 		contentPane.add(btnPerifricos);
-		
+
 		btnPlacaDeVdeo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
@@ -109,7 +109,7 @@ contentPane.setLayout(null);
 				f.setVisible(true);
 			}
 		});
-		
+
 		btnProcessador.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
@@ -117,7 +117,7 @@ contentPane.setLayout(null);
 				f.setVisible(true);
 			}
 		});
-		
+
 		btnHddSsd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
@@ -125,7 +125,7 @@ contentPane.setLayout(null);
 				f.setVisible(true);
 			}
 		});
-		
+
 		btnMemriaRam.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
@@ -133,7 +133,7 @@ contentPane.setLayout(null);
 				f.setVisible(true);
 			}
 		});
-		
+
 		btnPerifricos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
@@ -141,7 +141,7 @@ contentPane.setLayout(null);
 				f.setVisible(true);
 			}
 		});
-		
+
 		lblMonster.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
@@ -150,7 +150,7 @@ contentPane.setLayout(null);
 				f.setVisible(true);
 			}
 		});
-		
+
 		lblInformtica.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
